42b48fb2251833a52eab82a465294c95aaa85f88
Brian Wang <bwbrianwang1018@gmail.com>
2022-11-23 19:46:34 -0800

    Able to remove books from library

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let myLibrary = [
     new Book("Percy Jackson", "Rick Riordan", 282, true)
 ];
 
+const addBookForm = document.querySelector("#add-book-form");
+
 function Book(title, author, pages, hasRead) {
     this.title = title;
     this.author = author;
@@ -10,38 +12,63 @@ function Book(title, author, pages, hasRead) {
     this.hasRead = hasRead;
 }
 
-function addBookToLibrary() {
-    // do stuff here
+function render() {
+    // remove all books on screen
+    document.querySelectorAll(".book").forEach(book => {
+        book.remove();
+    });
+
+    // rerenders all books on screen
+    for (let i=0; i < myLibrary.length; i++) {
+        createBookElement(i, myLibrary[i].title, myLibrary[i].author, myLibrary[i].hasRead);
+    }
+
+    const removeButtons = document.querySelectorAll(".book button");
+    removeButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const parentDiv = button.parentNode;
+            const id = parentDiv.getAttribute("data-id");
+
+            myLibrary.splice(id, 1);
+            render();
+            console.log(myLibrary);
+            // document.querySelector("[data-id='" + id + "']").remove();
+        });
+    });
 }
 
-function createBookElement(title, author) {
+function createBookElement(id, title, author, hasRead) {
     const bookList = document.querySelector(".book-list")
     const newDiv = document.createElement("div");
-    const titleElement = document.createElement("p");
-    const authorElement = document.createElement("p");
-    titleElement.textContent = title;
-    authorElement.textContent = author;
+    const titleText = document.createElement("p");
+    const authorText = document.createElement("p");
+    const removeButton = document.createElement("button");
+    const hasReadCheckbox = document.createElement("input");
+
+    hasReadCheckbox.setAttribute("type", "checkbox");
+    newDiv.setAttribute("data-id", id);
+    titleText.textContent = title;
+    authorText.textContent = author;
+    removeButton.textContent = "Remove";
+    hasReadCheckbox.checked = hasRead;
     newDiv.classList.add("book")
-    newDiv.appendChild(titleElement);
-    newDiv.appendChild(authorElement);
+    newDiv.appendChild(titleText);
+    newDiv.appendChild(authorText);
+    newDiv.appendChild(removeButton);
+    newDiv.appendChild(hasReadCheckbox);
     bookList.appendChild(newDiv);
 }
 
-const addBookForm = document.querySelector("#add-book-form");
 addBookForm.addEventListener("submit", (e) => {
+    e.preventDefault();     // prevents the site from reloading when submitting the form
     const title = document.querySelector("#title").value;
     const author = document.querySelector("#author").value;
     const pages = document.querySelector("#pages").value;
     const read = document.querySelector("#read").checked;   // .checked returns true/false while value returns on/off
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
-    displayBooks();
-    e.preventDefault();     // prevents the site from reloading when submitting the form
     addBookForm.reset();    // resets form on submit
+    render();
 });
 
-function displayBooks() {
-    myLibrary.forEach(book => {
-        createBookElement(book.title, book.author)
-    });
-}
+render();
